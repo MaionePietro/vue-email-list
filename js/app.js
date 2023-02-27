90e786d15edc7ad0d2f431ff482976451bc690de
MaionePietro <pietromaionespam@gmail.com>
2023-02-27 17:12:55 +0100

    FIX

--- js/app.js
+++ js/app.js
@@ -10,9 +10,16 @@ createApp({
         getEmail() {
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
                 .then((res) => {
+                    console.log(res.data.email)
                     this.email.push(res.data.email)
                 })
         }
-    }
+    },
+    mounted() {
+		for (let i = 0; i < 10; i++) {
+			this.getEmail()
+		}
+	},
 }).mount('#app')
+
 
