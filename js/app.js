02c832a8ffe517cbd1ca9b724c61402781d7b99a
MaionePietro <pietromaionespam@gmail.com>
2023-02-28 10:30:02 +0100

    FEAT bonus

--- js/app.js
+++ js/app.js
@@ -10,16 +10,16 @@ createApp({
         getEmail() {
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
                 .then((res) => {
-                    console.log('email generata: '+res.data.response)
+                    console.log('email generata: ' + res.data.response)
                     this.email.push(res.data.response)
                 })
         }
     },
     mounted() {
-		for (let i = 0; i < 10; i++) {
-			this.getEmail()
-		}
-	},
+        for (let i = 0; i < 10; i++) {
+            this.getEmail()
+        }
+    },
 }).mount('#app')
 
 
